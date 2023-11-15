a85bac800ee17e7f2475c0bb1c0e40ba1fdc98b4
Beveky <im.beveky@gmail.com>
2023-11-15 13:01:54 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    person: {
+      fullName: "John Doe",
+      bio: "Hello everyone i hope you like my work",
+      imgSrc:
+        "https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D",
+      profession: "Full-stack developer",
+    },
+    show: false,
+    timeInterval: 0,
+  };
+
+  toggleProfile = () => {
+    this.setState((prevState) => ({
+      show: !prevState.show,
+    }));
+  };
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState((prevState) => ({
+        timeInterval: prevState.timeInterval + 1,
+      }));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    const { fullName, bio, imgSrc, profession } = this.state.person;
+    const { show, timeInterval } = this.state;
+
+    return (
+      <div className="App">
+        <button onClick={this.toggleProfile}>Toggle Profile</button>
+        {show && (
+          <div>
+            <h2>{fullName}</h2>
+            <p>{bio}</p>
+            <img src={imgSrc} style={{ maxWidth: "200px" }} alt={fullName} />
+            <p>Profession: {profession}</p>
+          </div>
+        )}
+        <p>Time Interval: {timeInterval} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Component/header.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#home">About</a>
+          </li>
+          <li>
+            <a href="#home">Download</a>
+          </li>
+          <li>
+            <a href="#home">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
